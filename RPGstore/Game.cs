ea2fcd9825cb061c671c3cff0ad47fc43c3ff5f9
mikeLofton <michaellofton32@gmail.com>
2021-09-20 16:59:30 -0500

    Set up some of the Game class functions

--- RPGstore/Game.cs
+++ RPGstore/Game.cs
@@ -5,9 +5,53 @@ using System.IO;
 
 namespace RPGstore
 {
+    struct Item
+    {
+        public string Name;
+        public int Cost;
+    }
+
     class Game
     {
+        private Player _player;
+        private Shop _shop;     
+        private bool _gameOver = false;
+        private int _currentScene = 0;
+
         public void Run()
+        {
+            Start();
+
+            while (!_gameOver)
+            {
+                Update();
+            }
+
+            End();
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void End()
+        {
+            Console.WriteLine("Have a nice day! :)");
+            Console.ReadKey(true);
+        }
+
+        private void InitializeItems()
+        {
+
+        }
+
+        private int GetInput(string description, params string[] options)
         {
 
         }
