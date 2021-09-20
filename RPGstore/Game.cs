7cdbd0e3f61ba5780ad70c5efc0b398d5833ecde
mikeLofton <michaellofton32@gmail.com>
2021-09-20 16:21:49 -0500

    Project start

---
+++ RPGstore/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace RPGstore
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
